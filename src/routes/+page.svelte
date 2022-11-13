86275017b8d9be4174cbb61702385a62e8cabfb8
WukoSiric <lstainer2002@gmail.com>
2022-11-14 00:39:53 +1030

    Removed import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" defer>
-    <link rel="stylesheet" href="../global.css" defer>
 </svelte:head>
 
 <!-- DEBUGGING -->
@@ -28,7 +27,7 @@
         <p>2048 is a single-player sliding tile puzzle video game written by Italian web developer Gabriele Cirulli and published on GitHub.</p>
     </div>
     <Board bind:tiles bind:has_lost bind:has_won bind:this={board}/>
-    <Footer />
+    <Footer --color="black" />
 </div>
 
 <!-- STYLE -->

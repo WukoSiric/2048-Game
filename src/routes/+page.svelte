ed84958db924247bf171a02dfa466d4fd17250e7
Luke Stainer <lstainer2002@gmail.com>
2022-11-14 14:32:55 +1030

    Added page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,6 +12,7 @@
 <!-- HTML START -->
 <svelte:head>
     <meta charset="utf-8">
+    <title>2048 Clone</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" defer>
 </svelte:head>

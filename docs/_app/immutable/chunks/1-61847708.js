c8259da11a8410a02039d002babb56a183e7642a
WukoSiric <lstainer2002@gmail.com>
2022-11-14 17:18:36 +1030

    Updated favicon

--- docs/_app/immutable/chunks/1-61847708.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-22e5a7c4.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-84d55e81.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-b80e2e54.js";export{t as component};

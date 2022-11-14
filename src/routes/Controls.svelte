42fb41f0aa6c77af68d95601063b147c889ead75
WukoSiric <lstainer2002@gmail.com>
2022-11-14 16:27:31 +1030

    Added dispatcher for controls

--- src/routes/Controls.svelte
+++ src/routes/Controls.svelte
@@ -4,28 +4,34 @@
     import {faArrowDown} from '@fortawesome/free-solid-svg-icons'
     import {faArrowLeft} from '@fortawesome/free-solid-svg-icons'
     import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
-    import {createEventDispatcher} from 'svelte';
+    import { createEventDispatcher } from 'svelte';
     import { Direction } from './types/Direction';
+    const dispatch = createEventDispatcher();
 
-    const dispatch = createEventDispatcher()
-
-    function trigger_key(direction: Direction) {
-        dispatch('key', direction)
-        console.log("Dispatched: ",direction)
+    function handleDirection(direction: Direction) {
+        if (direction === Direction.Up) {
+            dispatch("up")
+        } else if (direction === Direction.Down) {
+            dispatch("down")
+        } else if (direction === Direction.Left) {
+            dispatch("left")
+        } else if (direction === Direction.Right) {
+            dispatch("right")
+        }
     }
 </script>
 
 <div class="controls">
-    <div class="control" on:click={() => trigger_key(Direction.Up)}>
+    <div class="control" on:click={() => handleDirection(Direction.Up)}>
         <Fa icon={faArrowUp} size="3.5x"/>
     </div>
-    <div class="control" on:click={() => trigger_key(Direction.Down)}>
+    <div class="control" on:click={() => handleDirection(Direction.Down)}>
         <Fa icon={faArrowDown} size="3.5x"/>
     </div>
-    <div class="control" on:click={() => trigger_key(Direction.Left)}>
+    <div class="control" on:click={() => handleDirection(Direction.Left)}>
         <Fa icon={faArrowLeft} size="3.5x"/>
     </div>
-    <div class="control" on:click={() => trigger_key(Direction.Right)}>
+    <div class="control" on:click={() => handleDirection(Direction.Right)}>
         <Fa icon={faArrowRight} size="3.5x"/>
     </div>
 </div>

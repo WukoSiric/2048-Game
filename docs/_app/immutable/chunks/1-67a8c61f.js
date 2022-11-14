fb33c2c66ca54c5f969d80f485fef5155f318c11
WukoSiric <lstainer2002@gmail.com>
2022-11-14 17:26:39 +1030

    Changed favicon to .ico format

---
+++ docs/_app/immutable/chunks/1-67a8c61f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-435ef556.js";export{t as component};
--- docs/_app/immutable/chunks/1-84d55e81.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-b80e2e54.js";export{t as component};
